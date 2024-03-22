6744f67f5d8213641834c0da696a5660c11682af
Jeff Lai <79297365+Jefflai0315@users.noreply.github.com>
2024-03-22 10:29:43 +0800

    upload firebase

--- capture.py
+++ capture.py
@@ -1,5 +1,14 @@
 import time
 import picamera
+import firebase_admin
+
+from firebase_admin import initialize_app, storage
+from firebase_admin import db
+
+def initialize_firebase():
+
+
+
 
 def capture_image(file_path):
     with picamera.PiCamera() as camera:
@@ -14,5 +23,19 @@ def capture_image(file_path):
         print(f"Image captured and saved to: {file_path}")
 
 if __name__ == "__main__":
-    file_path = "/home/pi/image.jpg"  # Path to save the captured image
-    capture_image(file_path)
+        # Initialize Firebase Admin SDK
+    service_account_path = "intellidesk-174c9-firebase-adminsdk-garkf-abe9a9fb75.json"  # Path to your service account JSON file
+    firebase_config = {
+        'storageBucket': 'intellidesk-174c9.appspot.com'
+    }
+    file_name = "/home/pi/image.jpg"
+    local_file_path = "/home/pi/image.jpg"  # Local path to save the captured image
+    # Capture image here using picamera or another library
+    # Save the captured image to local_file_path
+    capture_image(local_file_path)
+
+    # Upload image to Firebase Storage
+    bucket = storage.bucket()
+    blob = bucket.blob(file_name)
+    blob.upload_from_filename(local_file_path)
+   
