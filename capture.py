ae792ecc0b45b8341b56593b618d748a6d6bdb94
Jeff Lai <79297365+Jefflai0315@users.noreply.github.com>
2024-03-22 11:02:33 +0800

    .

--- capture.py
+++ capture.py
@@ -14,7 +14,19 @@ from firebase_admin import db
 #     }
 #     initialize_app(credential=service_account_path, options=firebase_config)
 #     return db.reference()
+def capture_images(self,output_dir, num_images, interval):
+        # Create the output directory if it doesn't exist
 
+
+        # Capture images
+        for i in range(num_images):
+            # Wait for the specified interval
+            time.sleep(interval)
+            file_name = output_dir+ '/images'+str(i)+'.jpg'
+            capture_image(file_name)
+            bucket = storage.bucket()
+            blob = bucket.blob(file_name)
+            blob.upload_from_filename(local_file_path)
 
 
 def capture_image(file_path):
@@ -34,17 +46,30 @@ if __name__ == "__main__":
     cred = credentials.Certificate("intellidesk-174c9-firebase-adminsdk-garkf-abe9a9fb75.json")
     service_account_path = "intellidesk-174c9-firebase-adminsdk-garkf-abe9a9fb75.json"  # Path to your service account JSON file
     firebase_config = {
-        'storageBucket': 'intellidesk-174c9.appspot.com'
+        'storageBucket': 'intellidesk-174c9.appspot.com',
+        'databaseURL' : "https://intellidesk-174c9-default-rtdb.asia-southeast1.firebasedatabase.app/"
     }
     initialize_app(cred,firebase_config)
+    ref = db.reference()  # Path to the desired data in the Realtime Database
     file_name = "/home/pi/image.jpg"
     local_file_path = "/home/pi/image.jpg"  # Local path to save the captured image
     # Capture image here using picamera or another library
     # Save the captured image to local_file_path
-    capture_image(local_file_path)
+    while True: 
+        PostureCamera = ref.child('Controls/PostureCamera').get()
+        BiometricRecroding = ref.child('Controls/BiometricRecording').get()
+        if  BiometricRecroding == 1:
+            num_images = 5
+            interval = 3  # seconds
+            img_dir = "SetUpImages"
 
-    # Upload image to Firebase Storage
-    bucket = storage.bucket()
-    blob = bucket.blob(file_name)
-    blob.upload_from_filename(local_file_path)
-   
+        elif PostureCamera == 1:
+           
+            time.sleep(2)
+            capture_image(local_file_path)
+
+            # Upload image to Firebase Storage
+            bucket = storage.bucket()
+            blob = bucket.blob(file_name)
+            blob.upload_from_filename(local_file_path)
+    
